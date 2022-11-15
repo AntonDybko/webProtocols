c41a466fff6dbdba62515b9cffad5b9f5c3f7c30
adybko <dybko.anton11@ukr.net>
2022-11-15 22:11:06 +0100

    fix

--- lab6/client.js
+++ lab6/client.js
@@ -8,21 +8,37 @@ var config = {
     host:"sigma.ug.edu.pl",
     port:22,
     dstHost:"sigma.ug.edu.pl",
-    dstPort:3000,
+    dstPort:8080,
     localHost:'127.0.0.1',
     localPort: 3000
 };
+
+const printGame = (game) =>{
+    //let result = "-------------------"
+
+    console.log("-------------------")
+    Array.from(game).forEach(element => {
+        console.log("| ",element[0]," | ", element[1]," | ", element[2]," | ")
+        //result += ("| ",element[0]," | ", element[1]," | ", element[2]," | ")
+    });
+    console.log("-------------------")
+
+    //result += "-------------------"
+    //return result
+}
 
 var tnl = tunnel(config, function (error, server) {
     const gameId = axios.get("http://localhost:3000/").then(res => {
         const getine = res.data.split(" ")
         return getine[3]
     })
-    .then((id) => {
+    gameId.then((id) => {
         console.log(id)
-        axios.get(`http://localhost:3000/${gameId}`).then(res => {
+        axios.get(`http://localhost:3000/${id}`).then(res => {
             console.log(res.data)
-            
+            const game = res.data.split(":")
+            console.log(id)
+            //printGame(game[1])
             //console.log(id)
         })
     })
