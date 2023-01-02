057f782d2e7b6c890d8ed2bf87304b09a38f3a04
adybko <dybko.anton11@ukr.net>
2023-01-02 22:14:46 +0100

    lab11

--- lab11/ws/app.js
+++ lab11/ws/app.js
@@ -31,48 +31,134 @@ const server = mqtt.connect(host, options)
 
 const users = [];
 const chatRooms = []
-
-server.on("connect", function(socket){
-    //server.publish("loaded", chatRooms)
-    server.subscribe('load', { qos: 0 })
-    server.subscribe('roomManage', { qos: 0 })
-    server.subscribe('joinChat', { qos: 0 })
-    server.subscribe('leaveChat', { qos: 0 })
-    server.subscribe('twit', { qos: 0 })
-    
+server.on("connect", function(){
+    server.subscribe("onConnected")
+    server.subscribe("roomManage")
+    server.subscribe("twits")
+    server.subscribe("joinChat")
+    server.subscribe("leaveChat")
+    //server.subscribe('load')
+    //server.publish("onConneted", "gogo")
 })
 
-server.on("load", username => {
-    users.push({
-        socketId: socket.id,
-        userName: username
-    })
-})
-server.on("roomManage", arr => {
-    socket.join(arr[1])
-    chatRooms.push(arr[1])
-    io.to(arr[1]).emit("create", arr)
-})
-server.on("joinChat", chatname => {
-    socket.join(chatname)
-    let name = users.filter(u => u.socketId == socket.id)[0].userName
-    socket.to(chatname).emit("twits", [chatname, `(${name} joined room.)`])
-})
-server.on("leaveChat", async chatname => {
-    let name = users.filter(u => u.socketId == socket.id)[0].userName
-    socket.leave(chatname)
-    socket.to(chatname).emit("twits", [chatname, `(${name} left room.)`])
-    const sockets = await io.in(chatname).fetchSockets()
-    if(sockets.length ==0){
-        io.sockets.emit("deleteChat", chatname)
-        chatRooms.pop(chatname)
+server.on("message", (topic, string)=>{
+    //console.log(userId)
+    if(topic.toString()==='onConnected'){
+        //var uId = string
+        console.log(`${string}|onConnected`)
+        console.log(users)
+        console.log(chatRooms)
+        const userid = string.toString().split(',')
+        users.push({
+            socketId: userid[1],
+            userName: userid[0]
+        })
+        const chats = chatRooms.map(room =>{
+            return room.key
+        })
+        server.publish("loaded", chats.join('|'))
     }
-})
-server.on("twit", twit_chat => {
-    let name = users.filter(u => u.socketId == socket.id)[0].userName
-    io.to(twit_chat[1]).emit("twits", [twit_chat[1], `(${name}: ${twit_chat[0]})`])
+    if(topic.toString()==="roomManage"){
+        console.log(`roomManage: ${string.toString()}`)
+        chatRooms.push({
+            key:string.toString(),
+            value:[]
+        })
+        const chats = chatRooms.map(room =>{
+            return room.key
+        })
+        server.publish("loaded", chats.join('|'))
+    }
+    if(topic.toString()==="joinChat"){
+        const chat_id = string.toString().split('|')
+        console.log(`joinChat: ${chat_id[0]}`)
+        chatRooms.map(function(key_value){
+            console.log(`${key_value.key} => ${chat_id[0]}`)
+            if(key_value.key==chat_id[0]){
+                console.log(`${chat_id[1]}, => ${chat_id[0]}`)
+                key_value.value.push(chat_id[1])
+            }
+        }
+        )
+    }
+    if(topic.toString()==="leaveChat"){
+        const chat_id = string.toString().split('|')
+        console.log(`leaveChat: ${chat_id[0]}`)
+        chatRooms.map(function(key_value){
+            //console.log(`${key_value.key} => ${chat_id[0]}`)
+            if(key_value.key==chat_id[0]){
+                //console.log(`${chat_id[1]}, => ${chat_id[0]}`)
+                console.log(`${key_value.value} before leave `)
+                key_value.value = key_value.value.filter(x => x != chat_id[1])
+                console.log(key_value.value )
+            }
+        }
+        )
+    }
+    if(topic.toString()==="twits"){
+        console.log('twits')
+        const chat_id_twit = string.toString().split('|')
+        //console.log(chat_id_twit[0])
+        let chat = chatRooms.filter(key_value => key_value.key==chat_id_twit[0])[0]
+        console.log(chat.value)
+        if(chat.value.includes(chat_id_twit[1])){
+            server.publish(chat_id_twit[0], string)
+        }
+    }
+    /*if(topic.toString()==="twit"){
+        chatRooms.push(string)
+    }*/
+    /*server.on("twit", twit_chat => {
+            let name = users.filter(u => u.socketId == socket.id)[0].userName
+            io.to(twit_chat[1]).emit("twits", [twit_chat[1], `(${name}: ${twit_chat[0]})`])
+        })*/
+        /*server.on("message", (topic, string) => {
+            if(topic.toString()==='load'){
+                console.log(`${string}|load`)
+                const userid = string.toString().split(',')
+                users.push({
+                    socketId: userid[1],
+                    userName: userid[0]
+                })
+            }
+            if(topic.toString()==="roomManage"){
+                chatRooms.push(string)
+            }
+            if(topic==='joinChat'){
+                server.subscribe(string)
+            }
+            if(topic==="leaveChat"){
+                let name = users.filter(u => u.socketId == uId)[0].userName
+                server.unsubscribe(chatname)
+                const sockets = await io.in(chatname).fetchSockets()
+                if(sockets.length ==0){
+                    io.sockets.emit("deleteChat", chatname)
+                    chatRooms.pop(chatname)
+            }
+            }
+        })*/
+        /*server.on("joinChat", chatname => {
+            server.subscribe(chatname)
+            let name = users.filter(u => u.socketId == uId)[0].userName
+            server.publish(chatname, `${name} joined room.)`)
+            server.to(chatname).emit("twits", [chatname, `(${name} joined room.)`])
+        })*/
+        /*server.on("leaveChat", async chatname => {
+            let name = users.filter(u => u.socketId == uId)[0].userName
+            server.unsubscribe(chatname)
+            socket.to(chatname).emit("twits", [chatname, `(${name} left room.)`])
+            const sockets = await io.in(chatname).fetchSockets()
+            if(sockets.length ==0){
+                io.sockets.emit("deleteChat", chatname)
+                chatRooms.pop(chatname)
+            }
+        })*/
+        /*server.on("twit", twit_chat => {
+            let name = users.filter(u => u.socketId == socket.id)[0].userName
+            io.to(twit_chat[1]).emit("twits", [twit_chat[1], `(${name}: ${twit_chat[0]})`])
+        })*/
 })
 
-httpServer.listen(3000, function () {
+httpServer.listen(3001, function () {
  console.log('Serwer HTTP działa na pocie 3000');
 });
--- lab11/ws/public/js/skrypt.js
+++ lab11/ws/public/js/skrypt.js
@@ -1,29 +1,69 @@
 
-/*const clientId = 'mqttjs_' + Math.random().toString(16).substr(2, 8)
 
-const host = 'ws://broker.emqx.io:8083/mqtt'
 
-const options = {
-  keepalive: 60,
-  clientId: clientId,
-  protocolId: 'MQTT',
-  protocolVersion: 4,
-  clean: true,
-  reconnectPeriod: 1000,
-  connectTimeout: 30 * 1000,
-  will: {
-    topic: 'WillMsg',
-    payload: 'Connection Closed abnormally..!',
-    qos: 0,
-    retain: false
-  },
-}
 
-console.log('Connecting mqtt client')
-const client = mqtt.connect(host, options)*/
+
+//const username = prompt("Please enter a username: ", "");
 const username = prompt("Please enter a username: ", "");
+window.addEventListener("load", function (event) {
+  //const username = prompt("Please enter a username: ", "");
+  //client.publish("load", `${username},${clientId}`)
+  client.on("connect", function(){
+    console.log('connect')
+    client.subscribe('checkedTwit')
+    client.subscribe('loaded')
+    client.subscribe('deleteChat')
+    client.publish("onConnected", `${username},${clientId}`)
+    //client.publish("load", `${username},${clientId}`)
+    console.log(username)
+    console.log(clientId)
+  })
+
+});
+
 
 function createChat (chatname, chats){
+  if(chatname != ''){
+    client.subscribe(chatname)
+    console.log("roomManage try to pub")
+    client.publish("roomManage", chatname)
+    let li = document.createElement("li");
+    li.id = chatname
+    li.innerText = chatname;
+    chats.appendChild(li);
+    
+    let leavebutton = document.createElement("button");
+    leavebutton.innerHTML = "Leave";
+    leavebutton.onclick = function(){
+      li.removeChild(leavebutton)
+      let joinbutton = document.createElement("button");
+      joinbutton.innerHTML = "Join";
+      joinbutton.onclick = function(){
+        li.removeChild(joinbutton)
+        li.appendChild(leavebutton);
+        client.subscribe(chatname)
+        client.publish("joinChat", `${chatname}|${clientId}`)
+        client.publish('twits', `${chatname}|${clientId}|${username} joined ${chatname}`)
+      }
+      li.appendChild(joinbutton);
+      client.publish('twits', `${chatname}|${clientId}|${username} left ${chatname}`)
+      client.publish("leaveChat", `${chatname}|${clientId}`)
+      client.unsubscribe(chatname)
+      //client.unsubscribe(chatname)
+    }
+    li.appendChild(leavebutton);
+    client.publish("joinChat", `${chatname}|${clientId}`)
+    client.publish('twits', `${chatname}|${clientId}|${username} joined ${chatname}`)
+  }
+};
+
+function sendTwit (twit, chatname) {
+  console.log(twit, chatname)
+  client.publish('twits', `${chatname}|${clientId}|${twit}`)
+}
+
+
+/*function createChat (chatname, chats){
   //client.publish("roomManage",[username, chatname])
   let message = new Paho.MQTT.Message(`${username}||${chatname}`);
   message.destinationName = "roomManage";
@@ -66,7 +106,7 @@ function sendTwit (twit, chatname) {
   twitmsg.destinationName = "twit";
   twitmsg.qos = 0;
   client.send(twitmsg);
-}
+}*/
 
 
 
